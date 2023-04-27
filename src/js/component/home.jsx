25addaf00de506366400a28734a1379281282675
Juand30 <juandp2290@gmail.com>
2023-04-27 16:13:34 +0000

    actualizacion

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Form from "./Form";
-import Tarea from "./Tarea";
 
 const Home = () => {
-  const [tarea, setTarea] = useState("");
-  const [listadoTareas, setListadoTareas] = useState([]);
+  // const [tarea, setTarea] = useState("");
+  // const [listadoTareas, setListadoTareas] = useState([]);
   const [contadorTareas, setContadorTareas] = useState(0);
+  const [data, setData] = useState([]);
+ 
 
-  const contador = () => {
-    if (tarea != "") {
-      setListadoTareas([...listadoTareas, { label: tarea, done: false }]);
-      setContadorTareas(contadorTareas + 1);
-    }
-  };
+  // const contador = () => {
+  //   if (tarea != "") {
+  //     setListadoTareas([...listadoTareas, { label: tarea, done: false }]);
+  //     setContadorTareas(contadorTareas + 1);
+  //   }
+  // };
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -34,39 +35,16 @@ const Home = () => {
     console.log(listadoTareas);
   }
 
-  useEffect(()=>{
-    
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/juand') // Funcion GET predeterminada
-    .then(resp => {
-      return resp.json();
-    })
-    .then(data =>{
-      console.log(`Success:`, data);
-    })
-    .catch(error => {
-	    console.log("Hay un error");
-	    });
-
-  },[])
-
-  // useEffect(() => {
-  //   const options = {
-  //     method: 'PUT', 
-  //     body: JSON.stringify(data), 
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   };
-  
-  //   fetch('https://assets.breatheco.de/apis/fake/todos/user/juand', options)
-  //     .then(res => {
-  //       return res.json();
-  //     })
-  //     .then(response => console.log('Success:', response))
-  //     .catch(error => console.error(error));
-  // }, []);
-
-  // video de clase 18 min 51:13
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/juand")
+      .then((response) =>{
+      return response.json()})
+      .then((response) =>{
+        console.log("Respuesta:", response)
+        setData(response)
+      })
+      
+  }, []);
 
   function handleChange(e) {
     setTarea(e.target.value);
@@ -85,21 +63,14 @@ const Home = () => {
         <h1 className="letters">Todo-List</h1>
         <div className="tasks-container">
           <h2 className="letters">Lista de Tareas</h2>
-          <Form
-            tarea={tarea}
-            handleSubmit={handleSubmit}
-            handleChange={handleChange}
-          />
-          <div className="divtask letters contenedor-botones">
-            {listadoTareas.map((tarea) => (
-              <Tarea
-                key={tarea.id}
-                id={tarea.id}
-                tarea={tarea}
-                onBorrarTarea={onBorrarTarea}
-                contador={contador}
-              />
-            ))}
+          <div>
+         
+            {
+              data.map((response, index) =>{
+                return <li key={index}>{response.label}</li>
+              })
+            }
+           
           </div>
           <div className="contador">{contadorTareas} Item left</div>
         </div>
