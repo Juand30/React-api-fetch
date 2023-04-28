50c666827d61cfd38dfc3cb003fafb83fd48f7c4
Juand30 <juandp2290@gmail.com>
2023-04-28 11:31:53 +0000

    se agrega put

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,59 +2,60 @@ import React, { useEffect, useState } from "react";
 import Form from "./Form";
 
 const Home = () => {
-  // const [tarea, setTarea] = useState("");
-  // const [listadoTareas, setListadoTareas] = useState([]);
-  const [contadorTareas, setContadorTareas] = useState(0);
   const [data, setData] = useState([]);
- 
-
-  // const contador = () => {
-  //   if (tarea != "") {
-  //     setListadoTareas([...listadoTareas, { label: tarea, done: false }]);
-  //     setContadorTareas(contadorTareas + 1);
-  //   }
-  // };
+  const [tarea, setTarea] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
-
-    if (tarea === "") {
-      alert("Debes Agregar una Tarea");
-      return;
-    }
-
-    const nuevaTarea = {
-      id: Date.now(),
-      tarea: tarea,
-      completado: false,
-    };
-    const temp = [nuevaTarea, ...listadoTareas];
-    contador();
-    setListadoTareas(temp);
+  
+    // if (tarea === "") {
+    //   alert("Debes agregar una tarea");
+    //   return;
+    // }
+  
+    const nuevaTarea = { label: tarea, done: false };
+    setData([...data, nuevaTarea]);
     setTarea("");
-    console.log(listadoTareas);
+  
+    sendData(nuevaTarea);
   }
 
   useEffect(() => {
     fetch("https://assets.breatheco.de/apis/fake/todos/user/juand")
-      .then((response) =>{
-      return response.json()})
-      .then((response) =>{
-        console.log("Respuesta:", response)
-        setData(response)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("Respuesta:", response);
+        setData(response);
       })
-      
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
 
-  function handleChange(e) {
-    setTarea(e.target.value);
-    console.log("tarea");
+  function handleChange(event) {
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+
+    setData((prevData) => {
+      const newData = [...prevData];
+      newData[inputName].label = inputValue;
+      return newData;
+    });
   }
 
-  function onBorrarTarea(id) {
-    setContadorTareas(contadorTareas - 1);
-    const temp = listadoTareas.filter((item) => item.id !== id);
-    setListadoTareas(temp);
+  function sendData() {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/juand", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   return (
@@ -63,16 +64,13 @@ const Home = () => {
         <h1 className="letters">Todo-List</h1>
         <div className="tasks-container">
           <h2 className="letters">Lista de Tareas</h2>
+          <input type="text" name="0" onChange={handleChange} />
           <div>
-         
-            {
-              data.map((response, index) =>{
-                return <li key={index}>{response.label}</li>
-              })
-            }
-           
+            {data.map((data, index) => (
+              <div key={index}>{data.label}</div>
+            ))}
           </div>
-          <div className="contador">{contadorTareas} Item left</div>
+          <button onClick={handleSubmit}>Guardar Cambios</button>
         </div>
       </div>
     </>
